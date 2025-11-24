f385b197276403584a59e34e3c53d57f63f892c5
ShivaSai3697 <shivasai570@gmail.com>
2025-11-24 11:09:37 +0000

    Add Controller with hello

--- DemoController.java
+++ DemoController.java
@@ -1,8 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class DemoController {
 
     @GetMapping("/")
+    public String home() {
+        return "Hello from CI/CD Demo!";
+    }
+
+    @GetMapping("/hello")
     public String hello() {
-        return "Hello from Docker!";
+        return "Hello World!";
     }
 }
